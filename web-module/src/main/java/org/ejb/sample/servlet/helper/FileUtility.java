d3b962820e8fbf22622b1ffb7b61c194910f8bf7
867298 <867298@DESKTOP-B9FCMAL.cts.com>
2021-05-27 14:31:30 +0530

    Fix for remediation :FileSystem-Write-FileWriter - Write and Append

--- web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
+++ web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
@@ -2,17 +2,23 @@ package org.ejb.sample.servlet.helper;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import com.amazonaws.auth.*;
+import com.amazonaws.services.s3.*;
 
 public class FileUtility {
 
-        void generateReportFile(String content) throws IOException {
+    void generateReportFile(String content) throws IOException {
+        uploadS3ObjectAndGetUrl("bucketName", "/test/sample.txt", "keyName");
+    }
 
-            FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
-            fileWriter.write(content);
-            fileWriter.close();
-        }
+    private AmazonS3 getS3Client() {
+        AmazonS3 s3client;
+        AWSCredentials credentials = new BasicAWSCredentials(System.getenv("AWS_S3_ACCESS_KEY"), System.getenv("AWS_S3_SECRET_KEY"));
+    }
 
+    private URL uploadS3ObjectAndGetUrl(String bucketName, String uploadContentPath, String keyName) {
+        AmazonS3 s3client = getS3Client();
+        s3client.putObject(new PutObjectRequest(bucketName, keyName, new File(uploadContentPath)));
+        return s3client.getUrl(bucketName, keyName);
+    }
 }
-
-
-
